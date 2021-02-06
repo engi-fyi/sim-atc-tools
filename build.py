26745dd2d44427112226795677e639a802aeb856
Adam Hammond <adam@hammo.io>
2021-02-06 13:49:58 +1000

    Initial Commit
    Initial commit which includes data generator and site builder.

---
+++ build.py
@@ -0,0 +1,130 @@
+import sass
+import shutil
+import http.server
+import socketserver
+import os
+import watchgod
+import argparse
+import hashlib
+import shortuuid
+
+from pathlib import Path
+
+
+DIST = "./web/dist"
+WEB = "./web"
+LOCAL = "./web/local"
+SRC = "./web/src"
+DATA = "./data"
+CUSTOM = "./custom"
+CONFIG = "./config"
+
+
+def compile_css_assets(folder_name, prefix=""):
+    with open(f"{SRC}/styles.sass", "r") as style:
+        sass_in = style.read()
+        style.close()
+        css_out = sass.compile(string=sass_in)
+
+        with open(f"{WEB}/{folder_name}/{prefix}styles.css", "w") as compiled_style:
+            compiled_style.write(css_out)
+            compiled_style.close()
+
+
+def local():
+    print("Serving fresh copy of assets.")
+    print("Compiling CSS.")
+    compile_css_assets("local")
+    print("Copying HTML and JS.")
+    shutil.copy2(f"{SRC}/index.html", f"{LOCAL}/index.html")
+    shutil.copy2(f"{SRC}/app.js", f"{LOCAL}/app.js")
+    copy_data("local")
+    print("Web server running.")
+    serve_local(8080)
+
+
+def serve_local(port):
+    web_dir = os.path.join(os.path.dirname(__file__), LOCAL)
+    os.chdir(web_dir)
+
+    handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", port), handler)
+    print(f"Listening on port '{port}'.")
+    httpd.serve_forever()
+
+
+def copy_data(folder_name, force_fresh=False):
+    for m in ["airline", "aircraft", "airport"]:
+        source_file_name = f"{DATA}/{m}.json"
+        destination_file_name = f"{WEB}/{folder_name}/data/{m}.json"
+        Path(f"{WEB}/{folder_name}/data").mkdir(parents=True, exist_ok=True)
+        copy_file(source_file_name, destination_file_name, force_fresh)
+
+
+def delete_dist():
+    fid = os.listdir(DIST)
+
+    filtered_files = [f for f in fid if f.endswith(".html") or f.endswith(".js") or f.endswith(".css")]
+
+    for file in filtered_files:
+        path_to_file = os.path.join(DIST, file)
+        os.remove(path_to_file)
+        print(f"Deleting existing files at '{path_to_file}'.")
+
+
+def build():
+    delete_dist()
+    prefix = (shortuuid.uuid()[:7] + "_").lower()
+    print("Compiling CSS.")
+    compile_css_assets("dist", prefix)
+    print("Copying HTML and JS.")
+    shutil.copy2(f"{SRC}/app.js", f"{DIST}/{prefix}app.js")
+    build_index(prefix)
+    copy_data("dist")
+
+
+def build_index(prefix):
+    target_index = f"{DIST}/index.html"
+    copy_file(f"{SRC}/index.html", target_index)
+    f = open(target_index, "rt")
+    data = f.read()
+    f.close()
+
+    data = data.replace("app.js", f"{prefix}app.js")
+    data = data.replace("styles.css", f"{prefix}styles.css")
+
+    f = open(target_index, "wt")
+    f.write(data)
+    f.close()
+
+
+def copy_file(source_file_name, destination_file_name, force_fresh=False):
+    changes = True
+
+    if os.path.isfile(source_file_name):
+        if os.path.isfile(destination_file_name):
+            if not force_fresh:
+                source_hash = hashlib.md5(open(source_file_name, "rb").read()).hexdigest()
+                destination_hash = hashlib.md5(open(destination_file_name, "rb").read()).hexdigest()
+
+                if source_hash == destination_hash:
+                    changes = False
+
+        if changes:
+            print("There have been changes, copying new data files.")
+            shutil.copy2(source_file_name, destination_file_name)
+    else:
+        print("Sorry, the source file doesn't exist. Try 'generate' first.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Build tool for sim-atc.com.")
+    parser.add_argument("command", metavar="C", type=str, help="build, generate, local, or deploy")
+    args = parser.parse_args()
+
+    if args.command == "local":
+        watchgod.run_process(f"{SRC}", local, watcher_cls=watchgod.AllWatcher)
+    elif args.command == "build":
+        build()
+    else:
+        print("Sorry, that wasn't a valid command. Try again.")
---
+++ generator.py
@@ -0,0 +1,163 @@
+import requests
+import json
+import os
+
+from bs4 import BeautifulSoup
+from lunr import lunr
+
+CONFIGURATION_DIRECTORY = "config/"
+OUTPUT_DIRECTORY = "data/"
+CUSTOM_DIRECTORY = "custom/"
+BS4_PARSER_TYPE = "html.parser"
+
+
+class DataContainerDto:
+    def __init__(self):
+        self.data = {}
+        self.indexed = []
+
+
+def main():
+    for cm in get_configured_modules():
+        data = generate_data(cm)
+        write_data(cm, data)
+
+
+def get_configured_modules():
+    module_files = os.listdir(CONFIGURATION_DIRECTORY)
+    modules = []
+
+    for mf in module_files:
+        module = None
+        module_file_path = CONFIGURATION_DIRECTORY + mf
+
+        with open(module_file_path, "r") as f:
+            module = json.load(f)
+            modules.append(module)
+
+    return modules
+
+
+def write_data(module, data):
+    normal_data = {}
+    index_data = []
+
+    for d in data:
+        for k in d.data.keys():
+            if k not in normal_data:
+                normal_data[k] = []
+
+            for v in d.data[k]:
+                normal_data[k].append(v)
+                index_data.append(v)
+        # normal_data.update(d.data)
+
+    index_data.extend(d.indexed)
+
+    index_data_file = OUTPUT_DIRECTORY + "indexed_" + module["dataset_name"] + ".json"
+    idx = lunr(
+        ref="icao_code",
+        fields=module["fields"],
+        documents=index_data
+    )
+    serialized_idx = idx.serialize()
+    with open(index_data_file, "w") as idf:
+        idf.write(json.dumps(serialized_idx))
+        idf.close()
+
+    data_file = OUTPUT_DIRECTORY + module["dataset_name"] + ".json"
+    with open(data_file, "w") as df:
+        df.write(json.dumps(normal_data))
+        df.close()
+
+
+def generate_data(module):
+    data = []
+    if module["a_to_z"]:
+        print("Generating a to z data.")
+        ord_list = []
+
+        if module["a_to_z_has_0_to_9"]:
+            ord_list = ["0-9"] + list(range(ord("A"), (ord("Z") + 1)))
+        else:
+            ord_list = list(range(ord("A"), (ord("Z") + 1)))
+
+        for o in ord_list:
+            data.append(generate_a_to_z_data(module, o))
+
+    else:
+        print("Generating single page data.")
+        data.append(generate_single_page_data(module))
+
+    if module["has_custom"]:
+        data.append(load_custom_data(module))
+
+    return data
+
+
+def load_custom_data(module):
+    data = DataContainerDto()
+
+    custom_file_name = CUSTOM_DIRECTORY + module["dataset_name"] + ".json"
+    with open(custom_file_name) as f:
+        custom_data = json.load(f)
+
+        for k in custom_data.keys():
+            if k not in data.data:
+                data.data[k] = []
+
+            for v in custom_data[k]:
+                data.data[k].append(v)
+                data.indexed.append(v)
+
+        print(f"Added '{len(custom_data)}' custom record(s) to '" + module["dataset_name"] + "'.")
+
+    return data
+
+
+def generate_a_to_z_data(module, raw_letter):
+    if type(raw_letter).__name__ == "int":
+        letter = chr(raw_letter)
+    else:
+        letter = raw_letter
+
+    request_url = module["wikipedia_url"] + module["a_to_z_prefix"] + letter + module["a_to_z_postfix"]
+    return generate_single_page_data(module, request_url)
+
+
+def generate_single_page_data(module, request_url=None):
+    if request_url is None:
+        request_url = module["wikipedia_url"]
+
+    data = DataContainerDto()
+    page = requests.get(request_url)
+    print(f"'{request_url}' retrieved (status code: {page.status_code}).")
+
+    soup = BeautifulSoup(page.content, BS4_PARSER_TYPE)
+    table = soup.find("table", class_="wikitable")
+    table_body = table.find("tbody")
+    rows = table_body.find_all("tr")
+
+    for row in rows:
+        # 1 :: IATA, 2 :: ICAO, 3 :: Airline, 4 :: Call Sign, 5 :: Country, 6 :: Comments
+        cells = row.find_all("td")
+
+        if len(cells) >= len(module["fields"]):
+            icao_code_index = module["fields"].index("icao_code")
+            icao_code = cells[icao_code_index].get_text().rstrip()
+            if icao_code not in data.data:
+                data.data[icao_code] = []
+
+            data_container = {}
+
+            for i in range(0, len(module["fields"])):
+                data_container[module["fields"][i]] = cells[i].get_text().rstrip()
+
+            data.data[icao_code].append(data_container)
+            data.indexed.append(data_container)
+
+    return data
+
+
+if __name__ == "__main__":
+    main()
